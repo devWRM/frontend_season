5f72437ffe6198016453f9fbfd899b6a24a6f53a
devWRM <mcwen@comcast.net>
2021-09-24 11:04:45 -0400

    created TermContainer functional component

---
+++ src/components/TermContainer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import TermList from './TermList.js';
+
+function TermContainer() {
+    return (
+        <div>
+            <br />
+           
+           <TermList />
+        </div>
+    )
+}
+
+export default TermContainer;
