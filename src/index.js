c3a5ed2d727e66ca2a7a4221187f0b3d2db02f99
devWRM <mcwen@comcast.net>
2021-09-28 10:51:55 -0400

    wrapped app in Provider to give access to store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+
+
+import './index.css';
+import App from './App.js';
+import { rootReducer } from './reducers/rootReducer.js';
+
+
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+)
+
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
