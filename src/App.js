e20a60cdcebf5c805de6bd4918f45fc4de155950
devWRM <mcwen@comcast.net>
2021-10-01 10:11:36 -0400

    commented out or deleted refences to React logo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { connect } from 'react-redux';
 import { useEffect } from 'react';
@@ -26,7 +25,7 @@ function App(props) {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
