492b27578b3f56117ff6244259dd0646341abddc
devWRM <mcwen@comcast.net>
2022-02-20 13:19:13 -0500

    added comments clarifying that checkboxes do NOT show up until browser opened & return code edited then saved

--- src/components/TermList.js
+++ src/components/TermList.js
@@ -4,6 +4,9 @@ import { connect } from 'react-redux';
 import './TermList.css';
 
 
+    // READ NOTE Checkboxes are NOT automatically displaying when app first opened nor on page refresh
+    // READ NOTE Checkboxes only shows AFTER app is opened and TermList.js component's return code adjusted/altered & saved
+
 
 
 // STYLING RESOURCE     https://www.youtube.com/watch?v=5mHWWwbOu-s
@@ -56,8 +59,11 @@ class TermList extends Component {
     // }
 
 
-
-        checkClick = (e) => {
+    // https://reactjs.org/docs/react-component.html
+    // Constructor is the only place where you should assign this.state directly. 
+    // In all other methods, you need to use this.setState() instead.
+    // setState provided by React; checkClick built by SE
+    checkClick = (e) => {
             var {name, checked} = e.target;
             this.setState((e)=>{
                 var selectedSeason = e.Seasons
@@ -66,17 +72,17 @@ class TermList extends Component {
         };
 
 
-
+ 
 
         //  Deposit ALL the terms in a variable
         allTerms = this.props.terms.length && this.props.terms
         //  Map ALL the terms to create checkboxes that checkClick state on each
         // NOTE Checkboxes are NOT automatically displaying when app first opened nor on page refresh
-        // NOTE Checkboxes only shows AFTER app is opened and TermList.js component adjusted/altered
+        // NOTE Checkboxes only shows AFTER app is opened and TermList.js component's return code adjusted/altered & saved
 
                 termCheckboxes = this.props.terms.length && this.allTerms.map(term => {
                         return  <div key={term.id}>
-
+ 
                                 <input  type="checkbox"
                                         id={term.id}
                                         name={term.id}
@@ -107,16 +113,23 @@ class TermList extends Component {
         return (
             <div className="container">
                 <div class="child child-1">
-                    NEW Term List
+                    FILE: TermList.js <br />
+                    -- Checkboxes in middle box WILL NOT automatically display when app first opened nor on page refresh
+    <br />-- Checkboxes will show AFTER app is opened and TermList.js component's return code adjusted/altered & saved
+
                 </div>
 
 
+                {/* Displays in middle box: checkboxes populated with seasons from backend db/seeds.rb file */}
                 <div class="child child-2">
                     {this.termCheckboxes}
                 </div>
 
-
+                {/* Displays number of Season checked */}
                 <div class="child child-3">
+                    Without commas: {displayItems}
+                    <br /><br />
+                    With commas: 
                     {this.props.terms.length ? (displayItems + "")
                         :   ""
                     }
@@ -133,6 +146,8 @@ class TermList extends Component {
     }
 }
 
+
+// Assigns the state for terms to the variable terms
 const mapStateToProps = (state) => {
     return {terms: state.terms}
 }
